b8ad7c2a56f8794de22c313336b6d6a07ee2e704
Haebin Na <haebin@hive.local>
2026-07-02 19:28:14 -0700

    Bump cache-busting version to 20260703

--- ui/sw.js
+++ ui/sw.js
@@ -1,9 +1,9 @@
-const CACHE_NAME = 'squid-pwa-v20260701-001';
+const CACHE_NAME = 'squid-pwa-v20260703-001';
 const APP_SHELL = [
   '/',
   '/index.html',
-  '/style.css?v=20260701-001',
-  '/app.js?v=20260701-001',
+  '/style.css?v=20260703-001',
+  '/app.js?v=20260703-001',
   '/vendor/highlight.min.js',
   '/vendor/atom-one-dark.min.css',
   '/vendor/marked.min.js',
